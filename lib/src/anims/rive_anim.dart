3a8d6eef3e4fa59be149c7fbe42867595affab7b
romanpr <rm24052020@gmail.com>
2025-09-04 13:27:43 +0300

    rrr

--- lib/src/anims/rive_anim.dart
+++ lib/src/anims/rive_anim.dart
@@ -20,21 +20,36 @@ class RiveAssetAnimation extends StatefulWidget {
 }
 
 class _RiveAnimationState extends State<RiveAssetAnimation> {
+  bool canShow = false;
   // Controller for playback
-  late RiveAnimationController<dynamic> _controller;
-
-  @override
+   late RiveWidgetController _controller ;
+   late File _file;
+   @override
   void initState() {
     super.initState();
+    _getFile();
+  }
 
-    _controller = SimpleAnimation(widget.animName);
+  Future <void> _getFile ()async{
+   _file = await FileLoader.fromAsset(widget.assetPath, riveFactory: Factory.flutter).file();
+   _controller =  RiveWidgetController(_file);
+   canShow = true;
+   setState(() {
+
+   });
   }
 
   @override
   Widget build(BuildContext context) {
-    return RiveAnimation.asset(
-      widget.assetPath,
-      controllers: [_controller],
-    );
+    if(canShow){
+      return RiveWidget(
+        controller: _controller,
+      );
+    }
+    return Text('Wait please...');
+    // return RiveAnimation.asset(
+    //   widget.assetPath,
+    //   controllers: [_controller],
+    // );
   }
 }
