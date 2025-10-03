907bb944c0c3984d72b442e69535204b9b8f9fc6
romanpr <rm24052020@gmail.com>
2025-10-03 09:35:43 +0300

    replace riv to gif

--- lib/src/anims/rive_anim.dart
+++ lib/src/anims/rive_anim.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:gif/gif.dart';
+// import 'package:rive/rive.dart';
 
 ///RiveRuntimeRender
 class RiveAssetAnimation extends StatefulWidget {
@@ -21,20 +22,27 @@ class RiveAssetAnimation extends StatefulWidget {
 
 class _RiveAnimationState extends State<RiveAssetAnimation> {
   // Controller for playback
-  late RiveAnimationController<dynamic> _controller;
+  // late RiveAnimationController<dynamic> _controller;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = SimpleAnimation(widget.animName);
+    // _controller = SimpleAnimation(widget.animName);
   }
 
   @override
   Widget build(BuildContext context) {
-    return RiveAnimation.asset(
-      widget.assetPath,
-      controllers: [_controller],
+    String gif = "${widget.assetPath.split('.').first}.gif";
+    return ClipOval(
+      child: Gif(
+        image: AssetImage(gif),
+        duration: const Duration(seconds: 1),
+        autostart: Autostart.loop,
+        placeholder: (context) => const Text('Loading...'),
+      
+      ),
     );
+
   }
 }
